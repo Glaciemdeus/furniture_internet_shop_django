a3ce3b74fa4f6d4b8fa3016ff76b9645c6851114
Glaciemdeus <101183261+Glaciemdeus@users.noreply.github.com>
2024-03-06 07:58:22 +0300

    Add products db and admin panel

--- goods/models.py
+++ goods/models.py
@@ -10,3 +10,20 @@ class Categories(models.Model):
     
     class Meta:
         db_table = 'category'
+        
+        
+        
+        
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+    discription = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True)
+    price = models.DecimalField(default=0.00, max_digits = 7, decimal_places=2)
+    discount = models.DecimalField(default=0.00, max_digits = 7, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE)
+    
+    
+    class Meta:
+        db_table = 'product'
