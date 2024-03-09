4ff1b473a3b1f1d25a7b9fc6f6273bd38f96cd12
Glaciemdeus <101183261+Glaciemdeus@users.noreply.github.com>
2024-03-09 16:01:06 +0300

    Add admin slug autofill, add products and categories to the db

--- goods/models.py
+++ goods/models.py
@@ -12,6 +12,9 @@ class Categories(models.Model):
         db_table = 'category'
         
         
+    def __str__(self):
+        return self.name
+        
         
         
 class Products(models.Model):
@@ -20,10 +23,14 @@ class Products(models.Model):
     discription = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='goods_images', blank=True, null=True)
     price = models.DecimalField(default=0.00, max_digits = 7, decimal_places=2)
-    discount = models.DecimalField(default=0.00, max_digits = 7, decimal_places=2)
+    discount = models.DecimalField(default=0.00, max_digits = 4, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(to=Categories, on_delete=models.CASCADE)
     
     
     class Meta:
         db_table = 'product'
+        
+        
+    def __str__(self):
+        return f"{self.name} Количество - {self.quantity}"
